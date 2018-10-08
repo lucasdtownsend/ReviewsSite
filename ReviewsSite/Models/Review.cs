8a5f568274940c66eef30af3de7f3dedad50b6ef
lucasdtownsend <lucasdtownsend@gmail.com>
2018-10-08 14:53:48 -0400

    initial commit

---
+++ ReviewsSite/Models/Review.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string Category { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ ReviewsSite/ReviewRepository.cs
@@ -0,0 +1,29 @@
+﻿using ReviewsSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        Dictionary<int, Review> allReviews = new Dictionary<int, Review>()
+        {
+            { 1, new Review() {Id = 1, Title = "The First One"} },
+            { 2, new Review() {Id = 2, Title = "The Second One"} },
+            { 3, new Review() {Id = 3, Title = "The Third One"} }
+        };
+
+        public Review FindById(int Id)
+        {
+            return allReviews[Id];
+        }
+
+        public Dictionary<int, Review> GetAll()
+        {
+            return allReviews;
+        }
+
+    }
+}
